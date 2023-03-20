22108b74d2a131a65d14d0f96f05df468c84d5a8
rheapinto30 <94971999+rheapinto30@users.noreply.github.com>
2023-03-21 00:23:38 +0530

    navbar&footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<head>
+    <!--CSS-->
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<div class="footer">
+    <div class="container-fluid text-center">
+        <div class="content">
+            &copy; 2023. &nbsp; Made by <b> Rhea Pinto.</b>
+            <p class="m-0 pt-1 sf">THE SPARKS FOUNDATION</p>
+        </div>
+    </div>
+</div>
---
+++ navbar.php
@@ -0,0 +1,40 @@
+<head>
+    <!--Font Styles-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cookie&family=Merriweather:ital,wght@0,400;1,400;1,700&family=Roboto+Serif&display=swap" rel="stylesheet">
+
+    <!--Font Awesome Icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--CSS-->
+    <link rel="stylesheet" href="css/navbar.css">
+</head>
+
+
+<!-- Header -->
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid py-2">
+        <a class="navbar-brand nav-title" href="index.php"><i class="fa-solid fa-building-columns"></i> THE SPARKS BANK</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=" navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto ">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php"><i class="fa-solid fa-building-columns"></i> Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="allusers.php"><i class="fa-solid fa-money-bill-transfer"></i> Transfer Money </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="transactionhist.php"><i class="fa-solid fa-file-invoice"></i> Transaction History</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
